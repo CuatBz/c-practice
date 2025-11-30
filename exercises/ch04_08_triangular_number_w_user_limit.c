cca9037b01cba59bc6e3a65d1ed549d762036ba5
Nathanael Moh <50465141+CuatBz@users.noreply.github.com>
2025-11-30 07:00:58 -0600

    Use user input to limit loop repetitions

---
+++ exercises/ch04_08_triangular_number_w_user_limit.c
@@ -0,0 +1,23 @@
+/* Program that allows user to decide how many triangular #s to calculate */
+#include <stdio.h>
+
+int main(void) {
+  int n, number, triangularNumber, counter, limit;
+
+  printf("How many triangular numbers do you want to calculate? ");
+  scanf("%i", &limit);
+
+  for (counter = 1; counter <= limit; ++counter) {
+    printf("What triangular number do you want? ");
+    scanf("%i", &number);
+
+    triangularNumber = 0;
+
+    for (n = 1; n <= number; ++n)
+      triangularNumber += n;
+
+    printf("Triangular number %i is %i\n\n", number, triangularNumber);
+  }
+
+  return 0;
+}
