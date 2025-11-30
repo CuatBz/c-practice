6b5e0e4e0d75e170cfe8527c6080f9e2e3868d80
Nathanael Moh <50465141+CuatBz@users.noreply.github.com>
2025-11-29 22:28:15 -0600

    Change while loop to do...while loop

--- samples/reverse_digits.c
+++ samples/reverse_digits.c
@@ -9,12 +9,13 @@ int main(void) {
   
   printf("%i reversed is: ", number);
 
-  while (number != 0) {
+  do {
     right_digit = number % 10;
     printf("%i", right_digit);
 
     number = number / 10;
   }
+  while (number != 0);
   printf("\n");
 
   return 0;
