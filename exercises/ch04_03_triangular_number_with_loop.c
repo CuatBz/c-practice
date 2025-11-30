123306834b329a245674d72c76efdb53c2b6fe23
Nathanael Moh <50465141+CuatBz@users.noreply.github.com>
2025-11-29 23:10:27 -0600

    Use for loop to print out triangular numbers

---
+++ exercises/ch04_03_triangular_number_with_loop.c
@@ -0,0 +1,13 @@
+/* This program prints out every fifth triangular number */
+#include <stdio.h>
+
+int main(void) {
+  int result;
+
+  printf("Number            Triangular Number\n");
+  for (int i = 5; i <= 50; i += 5) {
+    result = i * (i + 1) / 2;
+    printf("%3i                    %4i\n", i, result);
+  }
+  return 0;
+}
